6dd15a1de7329c8ed9446168c2707c47e0f70625
ashraf7ussein <ashrafhussein1997@gmail.com>
2025-05-03 15:06:47 -0700

    implement navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,60 @@
+import { useState } from "react";
+import { IoClose } from "react-icons/io5";
+import { Link } from "react-router-dom";
+import { GiSkullCrack } from "react-icons/gi";
+import { HiOutlineMenu } from "react-icons/hi";
+
+const navLinks = [
+  { name: "home", href: "/" },
+  { name: "about", href: "/about" },
+  { name: "projects", href: "/projects" },
+  { name: "contact me", href: "/contact" },
+];
+
+const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  return (
+    <nav className="flex flex-wrap items-center justify-between bg-primary px-4 py-6 rounded-b-2xl">
+      <Link to="/" className="flex items center gap-3">
+        <GiSkullCrack className="text-white text-4xl mx-3 md:m-0" />
+        <span className="text-white text-2xl font-medium hidden md:block relative top-[5px]">
+          Ashraf.
+        </span>
+      </Link>
+
+      <button
+        onClick={() => setMenuOpen(!menuOpen)}
+        className="inline-flex w-10 h-10 justify-center items-center rounded-lg md:hidden
+         hover:bg-secondary cursor-pointer transition-all duration-300"
+      >
+        {menuOpen ? (
+          <IoClose className="text-white text-3xl" />
+        ) : (
+          <HiOutlineMenu className="text-white text-3xl" />
+        )}
+      </button>
+
+      <div
+        className={`w-full transition-all duration-300 ease-in-out
+        ${menuOpen ? "max-h-96 opacity-100" : "max-h-0 opacity-0"}
+        md:max-h-none md:opacity-100 md:block md:w-auto`}
+      >
+        <ul className="font-medium capitalize flex flex-col md:flex-row text-white gap-1 text-xl">
+          {navLinks.map((link) => (
+            <li
+              key={link.name}
+              className="hover:bg-secondary transition-all duration-300 px-4 py-2 rounded-xl"
+            >
+              <Link to={link.href} onClick={() => setMenuOpen(!menuOpen)}>
+                {link.name}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
--- src/routing/Layout.tsx
+++ src/routing/Layout.tsx
@@ -1,9 +1,10 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "../components/Navbar";
 
 const Layout = () => {
   return (
     <>
-      <p>nav bar </p>
+      <Navbar />
       <div id="main">
         <Outlet />
       </div>
